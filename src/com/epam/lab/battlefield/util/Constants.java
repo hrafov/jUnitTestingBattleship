10f2930c636cdd552261a789d7092730c1fde4db
Yevhen Hrafov <hrafovyevhen@gmail.com>
2019-11-17 15:36:08 +0200

    c1

--- src/com/epam/lab/battlefield/util/Constants.java
+++ src/com/epam/lab/battlefield/util/Constants.java
@@ -6,6 +6,7 @@ public class Constants {
 	public static final int DAMAGED = 1;
 	public static final int HIT = 2;
 	public static final int SHIPS_IN_BATTLE = 5;
+	public static final int NOT_RALLY_BIG_MAGIC_NUMBER_OF_ITERATIONS = 1000;
 	
 	private Constants() {		
 	}
