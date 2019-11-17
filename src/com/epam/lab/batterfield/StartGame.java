13287eb766560816d500b8ad7fd76dd20ecdad7b
Yevhen Hrafov <hrafovyevhen@gmail.com>
2019-11-17 15:46:30 +0200

    c9

--- src/com/epam/lab/batterfield/StartGame.java
+++ src/com/epam/lab/batterfield/StartGame.java
@@ -5,14 +5,9 @@ package com.epam.lab.batterfield;
 
 public class StartGame {	
 	public static void main(String[] args) {
-
-		Battlefield b = new Battlefield();		
-		
-        // for testing
-		b.displayBattleFieldInConsole(b.carrier.getBodyOfShip(),
-				b.battleship.getBodyOfShip(), b.cruiser.getBodyOfShip(),
-				b.submarine.getBodyOfShip(), b.destroyer.getBodyOfShip());
-
+		Battlefield b = new Battlefield();
+		View view = new View();		
+		view.displayBattleFieldInConsole(b); // for testing		
 		DialogWithComputer dialog = new DialogWithComputer(b);
 		dialog.start();
 	}
